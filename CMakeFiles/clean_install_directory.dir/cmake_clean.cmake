a922e5ac4e9886d34bec4a7765d5857a479690ac
Chingan <2111956539@qq.com>
2025-07-13 17:03:42 +0800

    合并代码

---
+++ CMakeFiles/clean_install_directory.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clean_install_directory"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/clean_install_directory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
