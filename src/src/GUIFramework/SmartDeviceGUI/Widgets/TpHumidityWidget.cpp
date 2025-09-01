4a84027acf66388a49b7691a0ecea1a9b82e30ac
hywang <751784947@qq.com>
2025-09-01 14:48:22 +0800

    feat(): 完成湿度计填充部分绘制

--- src/src/GUIFramework/SmartDeviceGUI/Widgets/TpHumidityWidget.cpp
+++ src/src/GUIFramework/SmartDeviceGUI/Widgets/TpHumidityWidget.cpp
@@ -27,8 +27,8 @@ TpHumidityWidget::TpHumidityWidget(TpChildWidget *parent) : TpChildWidget(parent
     TpHumidityWidgetData *tempData = new TpHumidityWidgetData();
     data_ = tempData;
 
-    tempData->colorList.emplace_back(_RGB(34, 132, 255));
     tempData->colorList.emplace_back(_RGB(204, 242, 252));
+    tempData->colorList.emplace_back(_RGB(34, 132, 255));
 
     tempData->titleTextFont.setFontColor(_RGB(54, 59, 100), _RGB(54, 59, 100));
     tempData->titleTextFont.setFontSize(13);
@@ -167,9 +167,105 @@ bool TpHumidityWidget::onPaintEvent(TpObjectPaintEvent *event)
         arcX + arcRadius, arcY,         // 终点：圆弧右端点
         _RGB(116, 121, 150), tempData->lineWidth);
 
+    // 计算填充值百分比
+    float percentValue = 1.0 * (tempData->curValue - tempData->minValue) / (tempData->maxValue - tempData->minValue);
+
+    // 绘制填充渐变
+    // 构建渐变效果
+    int32_t colorListSize = tempData->colorList.size();
+
+    TpLinearGradient lineGradient;
+    for (int i = 0; i < colorListSize; ++i)
+    {
+        lineGradient.setColorAt(1.0 * i / (colorListSize - 1), tempData->colorList.at(i));
+    }
+
+    if (colorListSize > 0)
+    {
+        lineGradient.setStart(arcX, arcY + arcRadius);
+        lineGradient.setFinalStop(arcX, startY);
+        painter->setGradient(&lineGradient);
+    }
+
+    // 水滴总高度，用于计算填充绘制高度
+    int32_t humidituAllHeight = startY + arcY + arcRadius;
+    float valueHeight = percentValue * humidituAllHeight;
+    int32_t fillCircleRadius = arcRadius - tempData->lineWidth;
+
+    if (tempData->curValue <= tempData->minValue)
+    {
+        // 小于等于最小值；什么也不绘制
+    }
+    else if (tempData->curValue >= tempData->maxValue)
+    {
+        // 大于等于最大值，绘制完全填充
+        painter->filledPie(arcX, arcY, fillCircleRadius, 0, 180, _RGB(0, 0, 0));
+
+        // 绘制上半区域
+    }
+    else
+    {
+        if (valueHeight < arcRadius)
+        {
+            // 先计算弦与圆相交的两个点坐标
+            ItpPoint leftPoint, rightPoint;
+
+            // 弦的一半长度
+            float lineW = 0;
+
+            // 镂空一个三角形
+            HollowMask hollowMask;
+            HollowMask::PolygonHollow polygonHollow;
+
+            float startAngle = 0;
+            float endAngle = 0;
+            double cosValue = 0;
+
+            lineW = std::sqrt(std::pow(fillCircleRadius, 2) - std::pow(fillCircleRadius - valueHeight, 2));
+
+            leftPoint.x = arcX - lineW;
+            leftPoint.y = arcY + fillCircleRadius - valueHeight;
+
+            rightPoint.x = arcX + lineW;
+            rightPoint.y = arcY + fillCircleRadius - valueHeight;
+
+            // 在扇形基础上镂空一个三角形
+            polygonHollow.posintList.emplace_back(ItpPoint(arcX, arcY));
+            polygonHollow.posintList.emplace_back(ItpPoint(leftPoint.x, leftPoint.y));
+            polygonHollow.posintList.emplace_back(ItpPoint(rightPoint.x, rightPoint.y));
+            hollowMask.addPolygonHollow(polygonHollow);
+
+            // 余弦定理 计算弦两个顶点的角度
+            cosValue = (std::pow(lineW, 2) + std::pow(fillCircleRadius - valueHeight, 2) - std::pow(fillCircleRadius, 2)) / (2.0 * lineW * (fillCircleRadius - valueHeight));
+            float angle = std::acos(cosValue) * 180.0 / M_PI;
+            startAngle = 90 - angle;
+            endAngle = 90 + angle;
+
+            // 防止浮点误差超出范围
+            if (cosValue < -1.0)
+                cosValue = -1.0;
+            else if (cosValue > 1.0)
+                cosValue = 1.0;
+
+            painter->filledPie(arcX, arcY, fillCircleRadius, startAngle, endAngle, _RGB(0, 0, 0), hollowMask);
+        }
+        else
+        {
+            // 高度末尾处于上半区域
+            // 先绘制半圆
+            painter->filledPie(arcX, arcY, fillCircleRadius, 0, 180, _RGB(0, 0, 0));
+
+            if (valueHeight > arcRadius)
+            {
+            }
+        }
+    }
+
+    // 重置渐变
+    painter->setGradient(nullptr);
+
     // 绘制百分比文本
-    float percentValue = 100.0 * (tempData->curValue - tempData->minValue) / (tempData->maxValue - tempData->minValue);
-    tempData->percentFont.setText(TpString::number(int32_t(percentValue)) + "%");
+    tempData->percentFont.setText(TpString::number(int32_t(percentValue * 100)) + "%");
     int32_t percentTextWidth = tempData->percentFont.pixelWidth();
     int32_t percentTextHeight = tempData->percentFont.pixelHeight();
     int32_t percentTextX = (width() - percentTextWidth) / 2.0;
