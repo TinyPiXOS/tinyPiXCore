7656cd653dbd9adad9e4ec2b68824b15db8c92cd
Chingan <2111956539@qq.com>
2025-09-05 16:40:55 +0800

    PWM功能及测试程序

--- examples/ExternUtils/pwm/pwm.cpp
+++ examples/ExternUtils/pwm/pwm.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
-	TpHardwarePwm pwm(2);
+	TpList<tpUInt8>list=TpHardwarePwm::getPwmNumbers();
+	for(auto &it : list)
+	{
+		;
+	}
+	TpHardwarePwm pwm(0);	//默认打开通道0
+	if(pwm.open()<0)
+		return -1;
+	pwm.setPeriod(10000);
+	pwm.setDutyCycle(50);
+	while(1);
+	pwm.close();
 }
