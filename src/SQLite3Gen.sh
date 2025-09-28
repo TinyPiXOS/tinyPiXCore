501f62a6ff3ed17f0f72f8db55d85bb0d2339e65
hywang <751784947@qq.com>
2025-09-28 15:13:42 +0800

    feat(): 解决在app的run之前的update请求未处理问题

--- src/SQLite3Gen.sh
+++ src/SQLite3Gen.sh
@@ -1,3 +1,3 @@
-gcc -c ./src_p/ExternUtils/sqlite3.c -lpthread -ldl -o sqlite3.o
-ar -r ./depend_lib/libsqlite3.a sqlite3.o
+gcc -c ./src_p/TpExtUtils/sqlite3.c -lpthread -ldl -o sqlite3.o
+ar -r ./depend_lib/static/x86_64/libsqlite3.a sqlite3.o
 rm -rf sqlite3.o
