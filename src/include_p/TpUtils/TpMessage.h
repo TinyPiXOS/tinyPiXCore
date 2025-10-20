0cce4163d40114c019c256da7d42b486101a5b74
hywang <751784947@qq.com>
2025-10-20 09:02:53 +0800

    feat(): 提交TpMessgae头文件

--- src/include_p/TpUtils/TpMessage.h
+++ src/include_p/TpUtils/TpMessage.h
@@ -1,51 +1,85 @@
-/* liucy has been here，but nothing to see and nothing left ^_^!*/
-
-/*
-** Copyright (c) 2007-2021 By Alexander.King.
-**
-** Permission is hereby granted, free of charge, to any person obtaining a
-** copy of this software and/or associated documentation files (the
-** "Materials"), to deal in the Materials without restriction, including
-** without limitation the rights to use, copy, modify, merge, publish,
-** distribute, sublicense, and/or sell copies of the Materials, and to
-** permit persons to whom the Materials are furnished to do so, subject to
-** the following conditions:
-**
-** The above copyright notice and this permission notice shall be included
-** in all copies or substantial portions of the Materials.
-**
-** THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-** CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-** TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-** MATERIALS OR THE USE OR OTHER DEALINGS IN THE MATERIALS.
-*/
 #ifndef __TP_MESSAGE_H
 #define __TP_MESSAGE_H
 
 #include <TpUtils.h>
 #include "TpCDef.h"
 
-#define TP_MESSAGE_LENGTH	10240
+/**
+ * @def TP_MESSAGE_LENGTH
+ * @brief 消息队列的默认长度，值为10240
+ * @note 当构造函数参数length小于等于0时，将使用此默认值
+ */
+#define TP_MESSAGE_LENGTH 10240
 
-TP_DEF_VOID_TYPE_VAR(IPitpMsg);
-
+TP_DEF_VOID_TYPE_VAR(ITpMessageData);
+/**
+ * @class TpMessage
+ * @brief 线程安全的消息队列类，实现基于循环队列的消息存储和传递
+ * @details
+ * 该类提供了一个线程安全的消息队列，支持阻塞和非阻塞的消息发送与接收。
+ * 使用互斥锁保证多线程环境下的数据安全，采用循环队列结构提高内存使用效率。
+ * @note 该类非拷贝构造，不可复制
+ * @warning 在多线程环境下使用时应确保正确的生命周期管理
+ */
 class TpMessage
 {
 public:
-	TpMessage(int32_t length = TP_MESSAGE_LENGTH);
-	virtual ~TpMessage();
-public:
-	virtual bool send(ItpUserEvent *message);//message can be defined
-	virtual bool sendWait(ItpUserEvent *message);//until success
-	virtual bool recvWait(ItpUserEvent *message);//block mode
-	virtual bool recv(ItpUserEvent *message);//none block mode
-	virtual void clear();
-public:
-	IPitpMsg *msgSet;
-};
+    /**
+     * @brief 构造函数，初始化消息队列
+     * @param[in] length 消息队列长度，默认为TP_MESSAGE_LENGTH
+     * @note 如果length参数小于等于0，将使用TP_MESSAGE_LENGTH作为默认值
+     */
+    TpMessage(int32_t length = TP_MESSAGE_LENGTH);
+    
+    /**
+     * @brief 析构函数，释放消息队列资源
+     */
+    virtual ~TpMessage();
 
+public:
+    /**
+     * @brief 发送消息（非阻塞模式）
+     * @param[in] message 要发送的消息指针
+     * @return bool 发送成功返回true，失败返回false
+     * @retval true 消息成功加入队列
+     * @retval false 队列已满或消息指针为空
+     * @note 此函数立即返回，不会等待队列有空闲位置
+     */
+    virtual bool send(ItpUserEvent *message);
+    
+    /**
+     * @brief 发送消息（阻塞模式，直到成功）
+     * @param[in] message 要发送的消息指针
+     * @return bool 总是返回true（因为会一直重试直到成功）
+     * @warning 如果队列始终满，此函数将无限期阻塞
+     */
+    virtual bool sendWait(ItpUserEvent *message);
+    
+    /**
+     * @brief 接收消息（阻塞模式）
+     * @param[out] message 接收消息的缓冲区指针
+     * @return bool 接收成功返回true
+     * @note 如果队列为空，此函数将阻塞等待直到有消息到达
+     */
+    virtual bool recvWait(ItpUserEvent *message);
+    
+    /**
+     * @brief 接收消息（非阻塞模式）
+     * @param[out] message 接收消息的缓冲区指针
+     * @return bool 接收成功返回true，失败返回false
+     * @retval true 成功接收到消息
+     * @retval false 队列为空或消息指针为空
+     */
+    virtual bool recv(ItpUserEvent *message);
+    
+    /**
+     * @brief 清空消息队列
+     * @note 此操作会删除队列中的所有消息
+     */
+    virtual void clear();
+
+public:
+    ITpMessageData *data_;
+};
 
 #endif
