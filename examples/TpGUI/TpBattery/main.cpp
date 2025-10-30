2f8c5bc20aeb026cf82d42211acffb9c84b44fd5
hywang <751784947@qq.com>
2025-10-30 11:22:49 +0800

    feat(): 优化TpBattery绘制

--- examples/TpGUI/TpBattery/main.cpp
+++ examples/TpGUI/TpBattery/main.cpp
@@ -8,8 +8,6 @@ int32_t main(int32_t argc, char *argv[])
 	TpApp app(argc, argv);
 	TpMainWindow *vScreen = new TpMainWindow();
 	vScreen->setBackGroundColor(_RGBA(128, 128, 128, 255));
-	
-	
 
 	TpBattery *battertWidget = new TpBattery(vScreen);
 	battertWidget->setValue(50);
